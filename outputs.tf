8ea890c51a0b0a69b43a2b5a35ab8bcbc0e4d110
Philip Papurt <ginkoid@gmail.com>
2021-07-28 21:40:48 -0400

    docs: add descriptions to new terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,10 @@ output "submit_url" {
   description = "Public Cloud Run URL for submissions"
 }
 output "submit_name" {
-  value = google_cloud_run_service.submit.name
+  value       = google_cloud_run_service.submit.name
+  description = "Name of Cloud Run service for submissions"
 }
 output "submit_id" {
-  value = google_cloud_run_service.submit.id
+  value       = google_cloud_run_service.submit.id
+  description = "ID of Cloud Run service for submissions"
 }
